85f0a9bdc0914a6e0ae3e8cd54549e1127c2e454
hanthom <m.hantho@gmail.com>
2015-12-10 11:58:43 -0600

    Updated to add institution.json

--- public/app.js
+++ public/app.js
@@ -1,1 +1,42 @@
-angular.module('getEd', ['ui.router']);
+angular.module('getEd', ['ui.router']).config(function($stateProvider, $urlRouterProvider) {
+
+	$stateProvider.state('/home', {
+		url: '/home',
+		templateUrl: 'templates/homeTmpl.html',
+		controller: 'homeCtrl'
+	})
+	.state('/admin', {
+		url: '/admin',
+		templateUrl: 'templates/adminProfileTmpl.html',
+		controller: 'adminProfileCtrl'
+	})
+	.state('/userProfile', {
+		url: '/userProfile',
+		templateUrl: 'templates/userProfileTmpl.html',
+		controller: 'userProfileCtrl'
+	})
+	.state('/search', {
+		url: '/searchresults',
+		templateUrl: 'templates/searchTmpl.html',
+		controller: 'searchCtrl'
+	})
+	.state('/login', {
+		url: '/login',
+		templateUrl: 'templates/loginTmpl.html',
+		controller: 'loginCtrl'
+	})
+	.state('/register', {
+		url: '/register',
+		templateUrl: 'templates/registerTmpl.html',
+		controller: 'registerCtrl'
+	})
+	.state('/institution', {
+		url: '/institution',
+		templateUrl: 'templates/institutionTmpl.html',
+		controller: 'institutionCtrl'
+	});
+
+	$urlRouterProvider.otherwise('/home');
+
+})
+
---
+++ public/mainService.js
@@ -0,0 +1,5 @@
+angular.module('getEd').service('mainService', function($http) {
+
+	
+
+});
