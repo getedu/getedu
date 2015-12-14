e4967813b1f859db3cb066cb5c6777944cd03fad
hanthom <m.hantho@gmail.com>
2015-12-14 13:10:21 -0600

    Further updating templates for admin register

---
+++ public/services/adminRegisterService.js
@@ -0,0 +1,10 @@
+angular.module('getEd').service('adminRegisterService', function($http) {
+      
+	// this.getInstitutions = function(query) {
+	// 	var query = query ? '?q=' + query : '';
+	// 	return $http({
+	// 		method: 'GET',
+	// 		url: '/' + query,
+	// 	});
+	// }
+});
