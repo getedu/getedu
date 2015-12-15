f3089393facd991d1c71212d20366975bb0ae44b
hanthom <m.hantho@gmail.com>
2015-12-15 16:02:39 -0600

    Added drop down options for institutionProfile register page

--- server-assets/models/adminModel.js
+++ server-assets/models/adminModel.js
@@ -4,13 +4,13 @@ var institutionProfileSchema = new mongoose.Schema({
 	name: {type: String, required: true},
 	city: {type:String, required: true},
 	state: {type: String, required: true},
-	type: {type:String, enum: ['4-year', '2-year', 'Bootcamp','Graduate'], required: true},
+	type: {type:String, enum: ['4-Year', '2-Year', 'Bootcamp','Graduate'], required: true},
 	studentCount: {type:Number, required: true},
-	topSubj: {type: String, enum: ['History', 'Science', 'Math', 'Social Studies', 'Language', 'Music'], required: true},
+	topSubj: {type: String, enum: ['History', 'Science', 'Math', 'Social Sciences', 'Language', 'Music'], required: true},
 	avgClassSize: {type: Number, required: true},
 	studentTeachRatio: {type: String, required: true},
 	genderRatio: {type: String, required: true},
-	financialAid: {type: String, required: true, enum: ['Need-blind', 'Need-based']}
+	financialAid: {type: String, required: true, enum: ['Need-blind', 'Need-based', 'No Financial Aid']}
 });
 
 module.exports = mongoose.model('InstitutionProfile', institutionProfileSchema);
