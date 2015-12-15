bc5648286e8d96d80d67aaef14cedba91dc38ca1
hanthom <m.hantho@gmail.com>
2015-12-15 13:10:00 -0600

    Added html code for both register pages

--- public/controllers/adminRegisterCtrl.js
+++ public/controllers/adminRegisterCtrl.js
@@ -1,9 +1,15 @@
 angular.module('getEd').controller('adminRegisterCtrl', function($scope, adminRegisterService) {
 	
+	$scope.selectedInst = '';
+
 	$scope.getInstitutions = function(search) {
 		adminRegisterService.getInstitutions(search).then(function(results) {
 			console.log(results.data);
 			$scope.institutions = results.data;
 		});
+	}
+
+	$scope.addAdminInfo = function() {
+
 	}
 });
--- server-assets/models/adminModel.js
+++ server-assets/models/adminModel.js
@@ -1,15 +1,15 @@
 var mongoose = require('mongoose');
 
 var adminSchema = new mongoose.Schema({
-	institutionName: {type: String, required: true},
+	name: {type: String, required: true},
 	city: {type:String, required: true},
 	state: {type: String, required: true},
-	type: {type:String, enum: ['4-year', '2-year', 'Bootcamp','Graduate']},
-	studentCount: {type:Number},
-	topSubj: {type: String, enum: ['History', 'Science', 'Math', 'Social Studies', 'Language', 'Music']},
-	avgClassSize: {type: Number},
-	studentTeachRatio: {type: String},
-	genderRatio: {type: String},
+	type: {type:String, enum: ['4-year', '2-year', 'Bootcamp','Graduate'], required: true},
+	studentCount: {type:Number, required: true},
+	topSubj: {type: String, enum: ['History', 'Science', 'Math', 'Social Studies', 'Language', 'Music'], required: true},
+	avgClassSize: {type: Number, required: true},
+	studentTeachRatio: {type: String, required: true},
+	genderRatio: {type: String, required: true},
 	financialAid: {type: String, required: true, enum: ['Need-blind', 'Need-based']}
 });
 
